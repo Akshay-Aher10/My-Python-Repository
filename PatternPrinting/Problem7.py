a83fc11ade9380a977bb780853bd273fbad7e5f2
Akshay Aher <103435669+Akshay-Aher10@users.noreply.github.com>
2022-10-09 09:01:21 +0530

    Add files via upload

---
+++ PatternPrinting/Problem7.py
@@ -0,0 +1,23 @@
+# 1   2   3   4   5
+
+# 1   2   3   4   5
+
+# 1   2   3   4   5
+
+# 1   2   3   4   5
+
+# 1   2   3   4   5
+
+def Display(no):
+
+    for i in range(1,no+1):
+        for j in range(1,no+1):
+            print(j,end="   ")
+        print("\n")    
+
+def main():
+    No1 = int(input("Enter Number\n"))
+    Display(No1)
+
+if __name__ == "__main__":
+    main()
